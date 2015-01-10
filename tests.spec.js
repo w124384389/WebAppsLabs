e25b723f3ada5b3e02be9def033e55cf45be26a1
Haris Skiadas <cskiadas@gmail.com>
2015-01-10 17:35:40 -0500

    Rename binSearch in tests too

--- tests.spec.js
+++ tests.spec.js
@@ -1,17 +1,17 @@
-describe('Your code for binSearch', function() {
+describe('Your code for binarySearch', function() {
    it('loads', function() {
 
    });
-   it('defines a binSearch function', function() {
-      expect(binSearch).to.exist();
-      expect(binSearch).to.be.a('function');
+   it('defines a binarySearch function', function() {
+      expect(binarySearch).to.exist();
+      expect(binarySearch).to.be.a('function');
    });
    it('works correctly on an empty array', function() {
-      expect(binSearch([], Math.random())).to.equal(false);
+      expect(binarySearch([], Math.random())).to.equal(false);
    });
    it('works correctly on values outside the array bounds', function() {
-      var f1 = function() { return binSearch([Math.random(), 3, 4], -2); }
-      var f2 = function() { return binSearch([Math.random(), 3, 4], 10); }
+      var f1 = function() { return binarySearch([Math.random(), 3, 4], -2); }
+      var f2 = function() { return binarySearch([Math.random(), 3, 4], 10); }
       expect(f1).to.not.throw(Error);
       expect(f2).to.not.throw(Error);
       expect(f1()).to.equal(false);
@@ -19,30 +19,30 @@ describe('Your code for binSearch', function() {
    });
    it('works correctly on a single element array', function() {
       var m = Math.random() * 10;
-      expect(binSearch([m], m + 1)).to.equal(false);
-      expect(binSearch([m], m)).to.equal(true);
+      expect(binarySearch([m], m + 1)).to.equal(false);
+      expect(binarySearch([m], m)).to.equal(true);
    });
    it('works correctly on a two-element array', function() {
       var m = Math.random() * 10;
-      expect(binSearch([m, m + 1], m + 1)).to.equal(true);
-      expect(binSearch([m, m + 1], m)).to.equal(true);
-      expect(binSearch([m, m + 1], m + 0.5)).to.equal(false);
+      expect(binarySearch([m, m + 1], m + 1)).to.equal(true);
+      expect(binarySearch([m, m + 1], m)).to.equal(true);
+      expect(binarySearch([m, m + 1], m + 0.5)).to.equal(false);
    });
    it('correctly finds middle element', function() {
       var arr = [Math.random(), Math.random() + 1, Math.random() + 2];
-      expect(binSearch(arr, arr[1])).to.equal(true);
+      expect(binarySearch(arr, arr[1])).to.equal(true);
    });
    it('works in cases where multiple elements are equal', function() {
       var m = Math.random() * 10;
-      expect(binSearch([m - 3, m, m, m + 3], m)).to.equal(true);
-      expect(binSearch([m - 3, m, m, m + 3], m + 1)).to.equal(false);
+      expect(binarySearch([m - 3, m, m, m + 3], m)).to.equal(true);
+      expect(binarySearch([m - 3, m, m, m + 3], m + 1)).to.equal(false);
    });
    it('correctly identifies misses in large arrays', function() {
       var N = 10000, arr = [];
       for (var i = 0; i < N; i += 1) { arr[i] = Math.random(); }
       arr.sort();
       for (var reps = 0; reps < 50; reps += 1) {
-         expect(binSearch(arr, Math.random())).to.equal(false);
+         expect(binarySearch(arr, Math.random())).to.equal(false);
       }
    });
    it('correctly identifies hits in large arrays', function() {
@@ -50,7 +50,7 @@ describe('Your code for binSearch', function() {
       for (var i = 0; i < N; i += 1) { arr[i] = Math.random(); }
       arr.sort();
       for (var reps = 0; reps < 50; reps += 1) {
-         expect(binSearch(arr, arr[Math.floor(Math.random() * 8000 + 500])))
+         expect(binarySearch(arr, arr[Math.floor(Math.random() * 8000 + 500)]))
             .to.equal(true);
       }
    });
@@ -66,7 +66,7 @@ describe('Your code for binSearch', function() {
          for (var i = 0; i < N; i += 1) { arr[i] = Math.random(); }
          arr.sort();
          for (var rep = 0; rep < reps; rep += 1) {
-            binSearch(arr, Math.random());
+            binarySearch(arr, Math.random());
          }
       });
       done();
