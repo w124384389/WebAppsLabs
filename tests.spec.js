23a3343c98ef951adcefb1b9fd72a3ddf12ede4e
Chen Wang <wangc16@hanover.edu>
2015-01-23 16:42:53 -0500

    Issue #9.

--- tests.spec.js
+++ tests.spec.js
@@ -22,6 +22,12 @@ describe('Your makeStack function', function() {
    var stack = makeStack();
 
    it('returns an object', function() {
-    expect(stack).to.be.a('object');
+      expect(stack).to.be.a('object');
+   });
+
+   it('returns an object with methods push, pop and isEmpty', function() {
+      ['push', 'pop', 'isEmpty'].forEach(function(key) {
+         expect(stack[key]).to.be.a('function');
+      });
    });
 });
