fae6f8bbc71b3f4551de3b5266042fcc860166ff
Chen Wang <wangc16@hanover.edu>
2015-01-31 15:57:43 -0500

    close #30

--- task.spec.js
+++ task.spec.js
@@ -10,3 +10,8 @@ expect = require('./chai.js').expect;
 Task = require('./task.js');
 
 // ADD YOUR TESTS HERE
+describe('Your code for tasks', function(){
+	it('defines a variable Task', function(){
+		expect(function(){Task;}).to.not.throw(Error);
+	});
+});
