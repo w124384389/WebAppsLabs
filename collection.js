737ddfda56b98ac3cea661111e3bba96014f9f0f
Chen Wang <wangc16@hanover.edu>
2015-02-01 20:36:27 -0500

    Implemented all the code for the TaskCollection methods. Close #55, Close #56

--- collection.js
+++ collection.js
@@ -8,12 +8,67 @@ var TaskCollection, Task, proto;
 
 Task = require("./task");
 
+// Helper method. You should not need to change it.
+// Use it in makeTaskFromString
+function searchInTasks(t, arr) {
+	var i, found = -1;
+	for (i = arr.length - 1; i >= 0; i -= 1) {
+		if (t instanceof RegExp) {
+			if (arr[ i ].title.match(t) != null) {
+				found = i;
+				break;
+			}
+		} else {
+			switch (typeof t) {
+				case "string":
+					if (arr[ i ].title === t) {
+						found = i;
+						break;
+					}
+					break;
+				
+				case "function":
+					if (t(arr[ i ])) {
+						found = i;
+						break;
+					}
+					break;
+
+				case "number": 
+					if (arr[ i ].id === t) {
+						found = i;
+						break;
+					}
+					break;
+			}
+		}
+	}
+	
+	return found;
+}
+
 /*
  *       Constructors
  */
 
 function makeNewCollection(arr) {
+	var col = Object.create(proto);
+	var tasks = [];
 
+	// if (Array.isArray(arr)) {
+	// 	for (var i = arr.length - 1; i >= 0; i -= 1) {
+	// 		col.add(arr[ i ]);
+	// 	};
+	// }
+
+   Object.defineProperty(col, 'values', {
+      enumerable: true,
+      configurable: true,
+      writeable: false,
+      value: tasks
+   });
+
+   return Object.preventExtensions(col);
 }
 
 
@@ -22,8 +77,70 @@ function makeNewCollection(arr) {
  */
 
 proto = {
-   //Add instance methods here
+	//Add instance methods here
+	length: function() {
+		return this.values.length;
+	},
+	isEmpty: function() {
+		return this.values.length === 0;
+	},
+	get: function(arg) {
+		var i = searchInTasks(arg, this.values);
+		return i === -1 ? null : this.values[ i ];
+	},
+	has: function(arg) {
+		return !(searchInTasks(arg, this.values) === -1);
+	},
+	add: function(t) {
+		var i;
+		if (Array.isArray(t)) {
+			for (i = t.length - 1; i >= 0; i -= 1) {
+				this.values.push(t[ i ]);
+			}
+		} else {
+			this.values.push(t);
+		}
 
+		return this;
+	},
+	new: function() {
+		var t = Task.new();
+		this.add(t);
+		return t;
+	},
+	remove: function(id) {
+		var i;
+		if (Array.isArray(id)) {
+			for (i = id.length - 1; i >= 0; i -= 1) {
+				if (searchInTasks(id[ i ], this.values) !== -1) {
+					this.values.splice(i, 1);
+				}
+			}
+		} else {
+			i = searchInTasks(id, this.values);
+			this.values.splice(i, 1);
+		}
+		return this;
+	},
+	filter: function(id) {
+		var i, newCol = TaskCollection.new();
+		if (Array.isArray(id)) {
+			for (i = id.length - 1; i >= 0; i -= 1) {
+				if (searchInTasks(id[ i ], this.values) !== -1) {
+					newCol.add(this.values[ i ]);
+					continue;
+				}
+			}
+		}
+		return newCol;
+	},
+	forEach: function(f) {
+		var i;
+		for (i = f.length - 1; i >= 0; i -= 1) {
+			f(this.values[ i ]);
+		}
+		return this;
+   }
 };
 
 
@@ -38,4 +155,4 @@ Object.defineProperty(TaskCollection, proto, {
    writable: false
 });
 
-module.exports = Task;
+module.exports = TaskCollection;
--- collection.spec.js
+++ collection.spec.js
@@ -11,3 +11,140 @@ Task = require('./task.js');
 TaskCollection = require('./collection.js');
 
 // ADD YOUR TESTS HERE
+describe('Your code for task collection', function(){
+	it('defines a variable TaskCollection', function(){
+		expect(function(){TaskCollection;}).to.not.throw(Error);
+	});
+});
+
+describe('Your makeNewCollection function', function(){
+	var col = TaskCollection.new();
+
+	it('returns an object', function() {
+		expect(col).to.be. a('object');
+	});
+
+	it('returns an object with methods length, isEmpty, get, has, add, new, remove, filter, forEach', function() {
+		['length', 'isEmpty', 'get', 'has', 'add', 'new', 'remove', 'filter', 'forEach'].forEach(function(key) {
+			expect(col[key]).to.be.a('function');
+		});
+	});
+});
+
+describe('TaskCollection methods', function(){
+	var col;
+
+	beforeEach(function() {
+		col = TaskCollection.new();
+	})
+
+	it('length returns the number of tasks currently stored.', function() {
+		col.add(Task.new()).add(Task.new());
+		expect(col.length()).to.equal(2);
+	});
+	it('isEmpty returns true when the collection is empty.', function() {
+		expect(col.isEmpty()).to.equal(true);
+	});
+	it('get returns the task that the title is a string', function() {
+		var t = [
+			Task.fromString("One #first #second #third"),
+			Task.fromString("Two #first #second"),
+			Task.fromString("Three #first"),
+		];
+		col.add(t);
+		expect(col.get("Two").title).to.equal("Two");
+	});
+	it('get returns the task that the ID is a number', function() {
+		var t = [
+			Task.fromString("One #first #second #third"),
+			Task.fromString("Two #first #second"),
+			Task.fromString("Three #first"),
+		];
+		col.add(t);
+		expect(col.get(7).id).to.equal(7);
+	});
+	it('get returns the task that accepts a function', function() {
+		var t = [
+			Task.fromString("One #first #second"),
+			Task.fromString("Two #first #second #third"),
+			Task.fromString("Three #first"),
+		];
+		col.add(t);
+		expect(col.get(function(task) { return task.hasTag("third") }).title).to.equal("Two");
+	});
+	it('get returns the task that the title is a regular expression', function() {
+		var t = [
+			Task.fromString("One #first #second #third"),
+			Task.fromString("12345 #first #second"),
+			Task.fromString("Three #first"),
+		];
+		col.add(t);
+		expect(col.get(/\d/).title).to.equal("12345");
+	});
+	it('has returns true if the task is in the collection.', function() {
+		var t = [
+			Task.fromString("One #first #second #third"),
+			Task.fromString("Two #first #second"),
+			Task.fromString("Three #first"),
+		];
+		col.add(t);
+		expect(col.has(16)).to.equal(true);
+	});
+	it('add returns the collection when the task is added.', function() {
+		expect(col.add(Task.new())).to.equal(col);
+	});
+	it('add returns the collection when added several tasks.', function() {
+		var t = [
+			Task.fromString("One #first #second #third"),
+			Task.fromString("Two #first #second"),
+			Task.fromString("Three #first"),
+		];
+		expect(col.add(t).length()).to.equal(3);
+	});
+	it('new returns the new empty task', function() {
+		expect(col.new().id).to.equal(22);
+	});
+	it('remove returns the collection when removes one task by ID', function() {
+		var t = [
+			Task.fromString("One #first #second #third"),
+			Task.fromString("Two #first #second"),
+			Task.fromString("Three #first"),
+		];
+		col.add(t);
+		expect(col.remove(1).length()).to.equal(2);
+	});
+	it('remove returns the collection when removes multiple tasks by IDs', function() {
+		var t = [
+			Task.fromString("One #first #second #third"),
+			Task.fromString("Two #first #second"),
+			Task.fromString("Three #first"),
+			Task.fromString("Four #fifth"),
+			Task.fromString("Five #first"),
+		];
+		col.add(t);
+		expect(col.remove([26,29]).length()).to.equal(3);
+	});
+	it('filter returns a new TaskCollection that contains the tasks that match the arguments ', function() {
+		var t = [
+			Task.fromString("One #first #second #third"),
+			Task.fromString("Two #first #second"),
+			Task.fromString("Three #first"),
+			Task.fromString("Four #fifth"),
+			Task.fromString("Five #first"),
+		];
+		col.add(t);
+		expect(col.filter([32,33]).length()).to.equal(2);
+	});
+	it('forEach returns the collection', function() {
+		var t = [
+			Task.fromString("One #first #second"),
+			Task.fromString("Two #first #second #third"),
+			Task.fromString("Three #first"),
+		];
+		col.add(t);
+		expect(col.forEach(function(task) { task.title = "whoa!" }).get("whoa!").title).to.equal("whoa!");
+	});
+
+});
+
+
