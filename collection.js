0a74d946c0667b4136fd1ead582c23e06fc0ae19
Chen Wang <wangc16@hanover.edu>
2015-02-13 13:58:12 -0500

    implemented get method and helper method searchInTasks, close #73, close #83

--- collection.js
+++ collection.js
@@ -7,6 +7,42 @@
 var TaskCollection, Task, proto;
 Task = require("./task");
 
+// Helper method. You should not need to change it.
+// Use it in makeTaskFromString
+function searchInTasks(t, arr) {
+	"use strict";
+	var i, found = -1;
+	for (i = arr.length - 1; i >= 0; i -= 1) {
+		if (t instanceof RegExp) {
+			if (arr[ i ].title.match(t) != null) {
+				found = i;
+				break;
+			}
+		} else {
+			switch (typeof t) {
+				case "string":
+					if (arr[ i ].title === t) {
+						found = i;
+						break;
+					}
+					break;
+				case "function":
+					if (t(arr[ i ])) {
+						found = i;
+						break;
+					}
+					break;
+				case "number":
+					if (arr[ i ].id === t) {
+						found = i;
+						break;
+					}
+					break;
+			}
+		}
+	}
+	return found;
+}
 
 /*
  *       Constructors
@@ -40,6 +76,11 @@ proto = {
 	isEmpty: function() {
 		"use strict";
 		return this.values.length === 0;
+	},
+	get: function(arg) {
+		"use strict";
+		var i = searchInTasks(arg, this.values);
+		return i === -1 ? null : this.values[ i ];
 	}
 };
 
