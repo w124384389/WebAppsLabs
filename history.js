ebc84b9f7d68684643d58fa808b495a6b823516f
Chen Wang <wangc16@hanover.edu>
2015-03-15 16:43:12 -0400

    implement command history, close #107, close #108, close #123, close #124, close #125,close #126,close #127,close #128close #129,

--- history.js
+++ history.js
@@ -4,9 +4,10 @@
  * Contains implementation for a CmdHistory "class"
  */
 
-var DLList, CmdHistory, proto;
+var Iterator, DLList, CmdHistory, proto;
 
 DLList = require("./dllist");
+Iterator = require("./iterator.js");
 
 /*
  *       Constructors
@@ -27,26 +28,63 @@ function makeNewHistory() {
 proto = {
    // Add instance methods here
    add: function(){
+		var item = {};
 
+		this.current.next = item;
+		item.prev = this.current;
+		item.next = null;
+		item.execute();
    },
    canRedo: function(){
-
-   },
+		if (this.current.next !== null){
+			return true;
+		}
+		return false;
+	},
    canUndo: function(){
-
+		if (this.current == null){
+			return false;
+		}
+		return true;
    },
    redo: function(){
-
+		if (!this.canRedo()){
+			throw new Error("There is no next item");
+		}
+		this.current = this.current.next;
+		this.current.execute();
    },
    undo: function(){
-
+		if (this.current == null){
+			throw new Error("no current item to unexecute");
+		}
+		this.current.unexecute();
+		this.current = this.current.prev;
    },
    undoableIterator: function(){
-
-   },
+		var it = this.current.next;
+		return Iterator.new(
+				function(){
+					it = it.prev;
+					return it;
+				},
+				function(){
+					return it.prev !== null;
+				}
+			);
+	},
    redoableIterator: function(){
-
-   }
+		var it = this.current.prev;
+		return Iterator.new(
+			function(){
+				it = it.next;
+				return it;
+			},
+			function(){
+				return it.next !== null;
+			}
+			);
+	}
 
 };
 
